211e2bc8b9d4f0d55013ad5bb5d75384dfd2aad8
Rathod Uday <167892846+UdayRathod5277@users.noreply.github.com>
2024-09-14 23:34:46 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,15 @@
+A = float(input("Enter the First digit: "))
+B = float(input("Enter the Second digit: "))
+o = str(input("Enter the Operation(+,-,*,/): "))
+if (o == '+'):
+    res = A + B
+elif (o == '-'):
+    res = A - B
+elif (o == '*'):
+    res = A * B
+elif (o == '/'):
+    res = A / B
+else:
+    print("ERROR: Invalid Input")
+print("RESULT: ",res)
+
